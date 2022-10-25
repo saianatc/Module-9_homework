0b5605ccc1a5cc58abf1df5b34534c6939d8d6f0
Saiana <111061312+saianatc@users.noreply.github.com>
2022-10-25 15:40:25 +0800

    Module 9. Homework

---
+++ Task 1.js
@@ -0,0 +1,40 @@
+let xmlString = <list>
+    <student>
+        <name lang="en">
+            <first>Ivan</first>
+            <second>Ivanov</second>
+        </name>
+        <age>35</age>
+        <prof>teacher</prof>
+    </student>
+    <student>
+        <name lang="ru">
+            <first>Петр</first>
+            <second>Петров</second>
+        </name>
+        <age>58</age>
+        <prof>driver</prof>
+    </student>
+</list>;
+
+let parser = new DOMParser();
+let result = [];
+
+const xmlDom = parser.parseFromString(xmlString, "text/xml");
+const students = xmlDOM.quarySelectorAll("student");
+
+students.forEach(node =>{
+    let student = {
+        name: node.querySelector("first").textContent + "" + node.querySelector("second").textContent,
+        age: node.querySelector("age").textContent,
+        prof: node.querySelector("name").textContent,
+        lang: node.querySelector("name").getAttribute("lang")
+    }
+    result.push(student);
+    });
+    console.log(result)
+)
+
+
+
+
